6bca818a30b0c192585c0e1c353095739ce0f952
goldietiara <goldietiara.acc@gmail.com>
2023-09-01 21:34:51 +0700

    git fixed grafbase

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,15 +6,15 @@ const isProduction = process.env.NODE_ENV === "production";
 // npx grafbase@0.24 dev
 // GRAFBASE
 const apiUrl = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL!
   : "http://127.0.0.1:4000/graphql";
 
 const apiKey = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY!
   : "henlo";
 
 const serverUrl = isProduction
-  ? process.env.NEXT_PUBLIC_SERVER_URL || ""
+  ? process.env.NEXT_PUBLIC_SERVER_URL!
   : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
